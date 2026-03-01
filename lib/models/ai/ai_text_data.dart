85b29853d67b216c999f3741d2384ec4fe250fb5
Abdelrhman-pi-3-14 <alianx796@gmail.com>
2026-03-01 09:29:14 +0200

    start working on the ai integration

---
+++ lib/models/ai/ai_text_data.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+
+class AiTextData {
+  String content = "";
+
+  String sender = "";
+  String time = "";
+
+  AiTextData({required this.sender, required this.content, required this.time});
+
+  Map<String, dynamic> toJson() {
+    return {'mode': 'text', "sender": sender, "content": content};
+  }
+
+  factory AiTextData.fromJson(Map<String, dynamic> json) {
+    return AiTextData(
+      sender: json['sender'],
+      content: json['content'],
+      time: json['time'],
+    );
+  }
+}
+class AiImageData {
+  String image = "";
+  String sender = "";
+  String time = "";
+
+  AiImageData({required this.sender, required this.image, required this.time});
+
+  Map<String, dynamic> toJson() {
+    return {'mode': 'text', "sender": sender, "image": image};
+  }
+
+  factory AiImageData.fromJson(Map<String, dynamic> json) {
+    return AiImageData(
+      sender: json['sender'],
+      image: json['image'],
+      time: json['time'],
+    );
+  }
+}
